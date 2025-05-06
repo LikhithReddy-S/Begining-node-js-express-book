6a57a1ad224bef36e4da9a4d8ff2c267c1eefee8
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-05-06 22:31:57 +0530

    deleted ch 9 10 need to do again

--- ch 9/middleware/validationMiddleware.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.files == null || req.body.title.trim() == '' || req.body.body.trim() == '') {
-        return res.redirect('/posts/new');
-    }
-    next()
-}
